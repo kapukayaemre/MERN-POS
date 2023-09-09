6755940c412235bc6b76c87458a3db751ed51d25
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-09-09 03:46:37 +0300

    Categories component created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
 import Header from "./components/header/Header";
+import Categories from "./components/categories/Categories";
 
 function App() {
     return (
-        <div>
+        <>
             <Header />
-            Home
-        </div>
+            <div className="home px-6 flex justify-between gap-10">
+                <div className="categories flex-1 overflow-auto max-h-[calc(100vh-_-112px)] pb-64">
+                    <div>
+                        <Categories />
+                    </div>
+                </div>
+                <div className="products flex-[8]">
+                    <div>products</div>
+                </div>
+                <div>
+                    <div>card totals</div>
+                </div>
+            </div>
+        </>
     );
 }
 
---
+++ client/src/components/categories/Categories.jsx
@@ -0,0 +1,63 @@
+
+const Categories = () => {
+    return (
+        <ul className="flex flex-col gap-4 text-center text-lg">
+            <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+                <span>Tümü</span>
+            </li>
+            <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+                <span>Yiyecek</span>
+            </li>
+            <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+                <span>İçecek</span>
+            </li>
+            <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+                <span>Tümü</span>
+            </li>
+            <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+                <span>Yiyecek</span>
+            </li>
+            <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+                <span>İçecek</span>
+            </li>
+            <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+                <span>Tümü</span>
+            </li>
+            <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+                <span>Yiyecek</span>
+            </li>
+            <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+                <span>İçecek</span>
+            </li>
+            <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+                <span>Tümü</span>
+            </li>
+            <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+                <span>Yiyecek</span>
+            </li>
+            <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+                <span>İçecek</span>
+            </li>
+            <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+                <span>Tümü</span>
+            </li>
+            <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+                <span>Yiyecek</span>
+            </li>
+            <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+                <span>İçecek</span>
+            </li>
+            <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+                <span>Tümü</span>
+            </li>
+            <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+                <span>Yiyecek</span>
+            </li>
+            <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+                <span>İçecek</span>
+            </li>
+        </ul>
+    );
+};
+
+export default Categories;
