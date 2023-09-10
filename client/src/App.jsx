5c7905eb5df4d61f659139da58212f23ca575894
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-09-10 14:09:24 +0300

    react-router-dom installed, routes created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,17 @@
-import Header from "./components/header/Header";
-import Categories from "./components/categories/Categories";
-import Products from "./components/products/Products";
-import CartTotals from "./components/cart/CartTotals";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CartPage from "./pages/CartPage";
+
 
 function App() {
     return (
         <>
-            <Header />
-            <div className="home px-6 flex md:flex-row flex-col justify-between gap-10 md:pb-0 pb-24">
-                <div className="categories overflow-auto max-h-[calc(100vh_-_112px)] md:pb-10">
-                    <div>
-                        <Categories />
-                    </div>
-                </div>
-                <div className="products flex-[8] max-h-[calc(100vh_-_112px)] overflow-auto pb-10">
-                    <div>
-                        <Products />
-                    </div>
-                </div>
-                <div className="cart-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border">
-                    <CartTotals />
-                </div>
-            </div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<HomePage/>}/>
+                    <Route path="/cart" element={<CartPage/>}/>
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,29 @@
+import Header from "../components/header/Header";
+import Categories from "../components/categories/Categories";
+import Products from "../components/products/Products";
+import CartTotals from "../components/cart/CartTotals";
+
+const HomePage = () => {
+    return (
+        <>
+            <Header />
+            <div className="home px-6 flex md:flex-row flex-col justify-between gap-10 md:pb-0 pb-24">
+                <div className="categories overflow-auto max-h-[calc(100vh_-_112px)] md:pb-10">
+                    <div>
+                        <Categories />
+                    </div>
+                </div>
+                <div className="products flex-[8] max-h-[calc(100vh_-_112px)] overflow-auto pb-10">
+                    <div>
+                        <Products />
+                    </div>
+                </div>
+                <div className="cart-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border">
+                    <CartTotals />
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default HomePage;
