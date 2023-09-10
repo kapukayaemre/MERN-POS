97ef0530fa021f07911072703414c49f48aa6df5
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-09-11 01:14:52 +0300

    Links created

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -9,14 +9,16 @@ import {
     LogoutOutlined
 } from "@ant-design/icons";
 
+import {Link} from "react-router-dom";
+
 const Header = () => {
     return (
         <div className="border-b mb-6">
             <header className="py-4 px-6 flex justify-between items-center gap-10">
                 <div className="logo">
-                    <a href="/">
+                    <Link to={"/"}>
                         <h2 className="text-2xl font-bold md:text-4xl">LOGO</h2>
-                    </a>
+                    </Link>
                 </div>
                 <div className="header-search flex-1 flex justify-center">
                     <Input
@@ -27,38 +29,38 @@ const Header = () => {
                     />
                 </div>
                 <div className="menu-links flex justify-between items-center gap-7 md:static fixed z-50 bottom-0 md:w-auto w-screen md:bg-transparent bg-white left-0 md:border-t-0 border-t md:px-0 px-4 py-1">
-                    <a href={"/"} className="menu-link flex flex-col hover:text-[#40a9ff]">
+                    <Link to={"/"} className="menu-link flex flex-col hover:text-[#40a9ff]">
                         <HomeOutlined className="flex justify-center md:text-2xl text-xl"/>
                         <span className="md:text-xs text-[10px]">Ana Sayfa</span>
-                    </a>
+                    </Link>
                     <Badge count={5} className="md:flex hidden">
-                        <a href={"/"} className="menu-link flex flex-col hover:text-[#40a9ff]">
+                        <Link to={"/cart"} className="menu-link flex flex-col hover:text-[#40a9ff]">
                             <ShoppingCartOutlined className="flex justify-center md:text-2xl text-xl"/>
                             <span className="md:text-xs text-[10px]">Sepet</span>
-                        </a>
+                        </Link>
                     </Badge>
-                    <a href={"/"} className="menu-link flex flex-col hover:text-[#40a9ff]">
+                    <Link to={"/"} className="menu-link flex flex-col hover:text-[#40a9ff]">
                         <CopyOutlined className="flex justify-center md:text-2xl text-xl"/>
                         <span className="md:text-xs text-[10px]">Faturalar</span>
-                    </a>
-                    <a href={"/"} className="menu-link flex flex-col hover:text-[#40a9ff]">
+                    </Link>
+                    <Link to={"/"} className="menu-link flex flex-col hover:text-[#40a9ff]">
                         <UserOutlined className="flex justify-center md:text-2xl text-xl"/>
                         <span className="md:text-xs text-[10px]">Müşteriler</span>
-                    </a>
-                    <a href={"/"} className="menu-link flex flex-col hover:text-[#40a9ff]">
+                    </Link>
+                    <Link to={"/"} className="menu-link flex flex-col hover:text-[#40a9ff]">
                         <BarChartOutlined className="flex justify-center md:text-2xl text-xl"/>
                         <span className="md:text-xs text-[10px]">İstatistikler</span>
-                    </a>
-                    <a href={"/"} className="menu-link flex flex-col hover:text-[#40a9ff]">
+                    </Link>
+                    <Link to={"/"} className="menu-link flex flex-col hover:text-[#40a9ff]">
                         <LogoutOutlined className="flex justify-center md:text-2xl text-xl"/>
                         <span className="md:text-xs text-[10px]">Çıkış</span>
-                    </a>
+                    </Link>
                 </div>
                 <Badge count={5} className="md:hidden flex">
-                    <a href={"/"} className="menu-link flex flex-col hover:text-[#40a9ff]">
+                    <Link to={"/cart"} className="menu-link flex flex-col hover:text-[#40a9ff]">
                         <ShoppingCartOutlined className="flex justify-center text-2xl"/>
                         <span className="md:text-xs text-[10px]">Sepet</span>
-                    </a>
+                    </Link>
                 </Badge>
             </header>
         </div>
