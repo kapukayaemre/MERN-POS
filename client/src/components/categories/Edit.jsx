ab8745af55998f23810d5ef5170cbb67761b9858
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-09-12 14:00:26 +0300

    Category delete process completed

--- client/src/components/categories/Edit.jsx
+++ client/src/components/categories/Edit.jsx
@@ -25,6 +25,23 @@ const Edit = ({ isEditModalOpen, setIsEditModalOpen, categories, setCategories }
         }
     }
 
+    const deleteCategory = (id) => {
+        if (window.confirm("Silmek İstediğinize Emin Misiniz?")) {
+            try {
+                fetch("http://localhost:8000/api/categories/delete-category", {
+                    method: "DELETE",
+                    body: JSON.stringify({categoryID: id}),
+                    headers: {"Content-type": "application/json; charset=UTF-8"}
+                });
+                message.success("Kategori Başarıyla Silindi!");
+                setCategories(categories.filter((item) => item._id !== id))
+            } catch (error) {
+                message.error("Bir Şeyler Yanlış Gitti...");
+                console.log(error)
+            }
+        }
+    }
+
     const columns = [
         {
             title: "Başlık",
@@ -44,12 +61,12 @@ const Edit = ({ isEditModalOpen, setIsEditModalOpen, categories, setCategories }
         {
             title: "İşlemler",
             dataIndex: "action",
-            render: (text, record) => {
+            render: (_, record) => {
                 return(
                     <div className="flex justify-between">
                         <Button type="link" onClick={() => setEditingRow(record)} className="pl-0" >Düzenle</Button>
                         <Button type="link" htmlType="submit" className="text-green-400">Kaydet</Button>
-                        <Button type="link" danger>Sil</Button>
+                        <Button type="link" danger onClick={() => deleteCategory(record._id)}>Sil</Button>
                     </div>
                 )
             }
