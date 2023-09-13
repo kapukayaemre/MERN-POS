3af251068bb8221ae05bcdc8f2efc50fc9a32a37
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-09-13 18:01:26 +0300

    Cart page completed

--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -1,42 +1,105 @@
 import Header from "../components/header/Header";
-import {Button, Card, Table} from "antd";
+import {Button, Card, message, Popconfirm, Table} from "antd";
 import {useState} from "react";
 import CreateBill from "../components/cart/CreateBill";
+import {useDispatch, useSelector} from "react-redux";
+import {MinusCircleOutlined, PlusCircleOutlined} from "@ant-design/icons";
+import {decrease, deleteCart, increase} from "../redux/cartSlice";
 
 const CartPage = () => {
 
     const [isModalOpen, setIsModalOpen] = useState(false);
-
-    const dataSource = [
-        {
-            key: '1',
-            name: 'Mike',
-            age: 32,
-            address: '10 Downing Street',
-        },
-        {
-            key: '2',
-            name: 'John',
-            age: 42,
-            address: '10 Downing Street',
-        },
-    ];
+    const cart = useSelector((state) => state.cart);
+    const dispatch = useDispatch();
 
     const columns = [
         {
-            title: 'Name',
-            dataIndex: 'name',
-            key: 'name',
+            title: 'Ürün Görseli',
+            dataIndex: 'img',
+            key: 'img',
+            width: "125px",
+            render: (text) => {
+                return (<img src={text} alt="" className="w-full h-20 object-cover"/>)
+            }
         },
         {
-            title: 'Age',
-            dataIndex: 'age',
-            key: 'age',
+            title: 'Ürün Adı',
+            dataIndex: 'title',
+            key: 'title',
         },
         {
-            title: 'Address',
-            dataIndex: 'address',
-            key: 'address',
+            title: 'Kategori',
+            dataIndex: 'category',
+            key: 'category',
+        },
+        {
+            title: 'Ürün Fiyatı',
+            dataIndex: 'price',
+            key: 'price',
+            render: (text) => {
+                return (<span>{text.toFixed(2)}₺</span>)
+            }
+        },
+        {
+            title: 'Ürün Adeti',
+            dataIndex: 'quantity',
+            key: 'quantity',
+            render: (text, record) => {
+                return (
+                    <div className="flex items-center">
+                        <Button
+                            type="primary"
+                            className="w-full flex items-center justify-center !rounded-full"
+                            size="small"
+                            icon={<PlusCircleOutlined/>}
+                            onClick={() => dispatch(increase(record))}
+                        />
+                        <span className="font-bold w-6 inline-block text-center">{record.quantity}</span>
+                        <Button
+                            type="primary"
+                            className="w-full flex items-center justify-center !rounded-full"
+                            size="small"
+                            icon={<MinusCircleOutlined/>}
+                            onClick={() => {
+                                if (record.quantity === 1) {
+                                    if (window.confirm("Ürün Silinsin mi?")) {
+                                        dispatch(decrease(record));
+                                        message.success(`${record.title} Sepetten Silindi!`);
+                                    }
+                                }
+
+                                if (record.quantity > 1) {
+                                    dispatch(decrease(record));
+                                }
+                            }}
+                        />
+                    </div>
+                )
+            }
+        },
+        {
+            title: 'Toplam Fiyat',
+            render: (text, record) => {
+                return (<span>{(record.quantity * record.price).toFixed(2)}₺</span>)
+            }
+        },
+        {
+            title: 'İşlemler',
+            render: (_, record) => {
+                return (
+                    <Popconfirm
+                        title={`${record.title} Ürününü Sepetten Silmek İstediğinize Emin misiniz?`}
+                        onConfirm={() => {
+                            dispatch(deleteCart(record));
+                            message.success(`${record.title} Sepetten Silindi!`);
+                        }}
+                        okText="Evet"
+                        cancelText="Hayır"
+                    >
+                        <Button type="link" danger>Sil</Button>
+                    </Popconfirm>
+                )
+            }
         },
     ];
 
@@ -45,7 +108,7 @@ const CartPage = () => {
         <>
             <Header/>
             <div className="px-6">
-                <Table dataSource={dataSource} columns={columns} bordered pagination={false}/>
+                <Table dataSource={cart.cartItems} columns={columns} bordered pagination={false}/>
                 <div className="cart-total flex justify-end mt-4">
                     <Card className="w-72">
                         <div className="flex justify-between">
