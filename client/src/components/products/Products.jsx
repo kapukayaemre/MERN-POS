908cc8806f618752ab91377224af364bb8c855b9
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-09-14 01:42:16 +0300

    Category filter added

--- client/src/components/products/Products.jsx
+++ client/src/components/products/Products.jsx
@@ -4,27 +4,13 @@ import {EditOutlined, PlusOutlined} from "@ant-design/icons";
 import Add from "./Add";
 import {useNavigate} from "react-router-dom";
 
-const Products = ({categories}) => {
-    const [products, setProducts] = useState([]);
+const Products = ({categories, filtered, products, setProducts}) => {
     const [isAddModalOpen, setIsAddModalOpen] = useState(false);
     const navigate = useNavigate();
 
-    useEffect(() => {
-        const getProducts = async () => {
-            try {
-                const res = await fetch("http://localhost:8000/api/products/get-all");
-                const data = await res.json();
-                setProducts(data);
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        getProducts();
-    }, []);
-
     return (
         <div className="products-wrapper grid grid-cols-card gap-4">
-            {products.map((item) => (
+            {filtered.map((item) => (
                 <ProductItem item={item} key={item._id}/>
             ))}
 
