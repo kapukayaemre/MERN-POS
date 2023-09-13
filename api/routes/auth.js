afc8a62e49d50e68a8c84698195ef7b1a42643c9
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-09-14 00:53:46 +0300

    api auth updated

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -29,7 +29,10 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
     try {
         const user = await User.findOne({email: req.body.email});
-        !user && res.status(404).send({ error: "Kullanıcı Bulunamadı" });
+
+        if (!user) {
+            return res.status(404).send({ error: "Kullanıcı Bulunamadı" });
+        }
 
         const validPassword = await bcrypt.compare(req.body.password, user.password);
 
