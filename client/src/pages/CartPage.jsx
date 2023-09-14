815f9f301a01e2900c8719248f29f5cff05e3119
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-09-14 15:31:32 +0300

    Cart page column search and filter feature added

--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -1,16 +1,134 @@
 import Header from "../components/header/Header";
-import {Button, Card, message, Popconfirm, Table} from "antd";
-import {useState} from "react";
+import {Button, Card, Input, message, Popconfirm, Space, Table} from "antd";
+import {useRef, useState} from "react";
 import CreateBill from "../components/cart/CreateBill";
 import {useDispatch, useSelector} from "react-redux";
-import {MinusCircleOutlined, PlusCircleOutlined} from "@ant-design/icons";
+import {MinusCircleOutlined, PlusCircleOutlined, SearchOutlined} from "@ant-design/icons";
 import {decrease, deleteCart, increase} from "../redux/cartSlice";
+import Highlighter from "react-highlight-words";
 
 const CartPage = () => {
 
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [searchText, setSearchText] = useState("");
+    const [searchedColumn, setSearchedColumn] = useState("");
+    const searchInput = useRef(null);
     const cart = useSelector((state) => state.cart);
     const dispatch = useDispatch();
+
+    const handleSearch = (selectedKeys, confirm, dataIndex) => {
+        confirm();
+        setSearchText(selectedKeys[0]);
+        setSearchedColumn(dataIndex);
+    };
+
+    const handleReset = (clearFilters) => {
+        clearFilters();
+        setSearchText("");
+    };
+
+    const getColumnSearchProps = (dataIndex) => ({
+        filterDropdown: ({
+                             setSelectedKeys,
+                             selectedKeys,
+                             confirm,
+                             clearFilters,
+                             close,
+                         }) => (
+            <div
+                style={{
+                    padding: 8,
+                }}
+                onKeyDown={(e) => e.stopPropagation()}
+            >
+                <Input
+                    ref={searchInput}
+                    placeholder={`Search ${dataIndex}`}
+                    value={selectedKeys[0]}
+                    onChange={(e) =>
+                        setSelectedKeys(e.target.value ? [e.target.value] : [])
+                    }
+                    onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                    style={{
+                        marginBottom: 8,
+                        display: "block",
+                    }}
+                />
+                <Space>
+                    <Button
+                        type="primary"
+                        onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                        icon={<SearchOutlined/>}
+                        size="small"
+                        style={{
+                            width: 90,
+                        }}
+                    >
+                        Search
+                    </Button>
+                    <Button
+                        onClick={() => clearFilters && handleReset(clearFilters)}
+                        size="small"
+                        style={{
+                            width: 90,
+                        }}
+                    >
+                        Reset
+                    </Button>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => {
+                            confirm({
+                                closeDropdown: false,
+                            });
+                            setSearchText(selectedKeys[0]);
+                            setSearchedColumn(dataIndex);
+                        }}
+                    >
+                        Filter
+                    </Button>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => {
+                            close();
+                        }}
+                    >
+                        close
+                    </Button>
+                </Space>
+            </div>
+        ),
+        filterIcon: (filtered) => (
+            <SearchOutlined
+                style={{
+                    color: filtered ? "#1890ff" : undefined,
+                }}
+            />
+        ),
+        onFilter: (value, record) =>
+            record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+        onFilterDropdownOpenChange: (visible) => {
+            if (visible) {
+                setTimeout(() => searchInput.current?.select(), 100);
+            }
+        },
+        render: (text) =>
+            searchedColumn === dataIndex ? (
+                <Highlighter
+                    highlightStyle={{
+                        backgroundColor: "#ffc069",
+                        padding: 0,
+                    }}
+                    searchWords={[searchText]}
+                    autoEscape
+                    textToHighlight={text ? text.toString() : ""}
+                />
+            ) : (
+                text
+            ),
+    });
 
     const columns = [
         {
@@ -26,11 +144,13 @@ const CartPage = () => {
             title: 'Ürün Adı',
             dataIndex: 'title',
             key: 'title',
+            ...getColumnSearchProps("title")
         },
         {
             title: 'Kategori',
             dataIndex: 'category',
             key: 'category',
+            ...getColumnSearchProps("category")
         },
         {
             title: 'Ürün Fiyatı',
@@ -38,7 +158,8 @@ const CartPage = () => {
             key: 'price',
             render: (text) => {
                 return (<span>{text.toFixed(2)}₺</span>)
-            }
+            },
+            sorter: (a, b) => a.price - b.price
         },
         {
             title: 'Ürün Adeti',
@@ -107,7 +228,8 @@ const CartPage = () => {
         <>
             <Header/>
             <div className="px-6">
-                <Table dataSource={cart.cartItems} columns={columns} bordered pagination={false} scroll={{ x:1200, y:300 }}/>
+                <Table dataSource={cart.cartItems} columns={columns} bordered pagination={false}
+                       scroll={{x: 1200, y: 300}}/>
                 <div className="cart-total flex justify-end mt-4">
                     <Card className="w-72">
                         <div className="flex justify-between">
